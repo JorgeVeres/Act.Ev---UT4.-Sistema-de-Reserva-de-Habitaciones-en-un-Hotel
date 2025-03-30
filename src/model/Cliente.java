c6814212ffd63b1e658b3de60ecf9a49bcb1bc28
Airam-28 <airamceballomarrero@gmail.com>
2025-03-30 15:46:51 +0100

    Modelo cliente creado

---
+++ src/model/Cliente.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+    private static int contadorId = 1;
+    private String id;
+    private String nombreCompleto;
+    private List<Reserva> reservasActivas;
+    private List<Reserva> historialReservas;
+
+    public Cliente(String nombreCompleto){
+        this.id = "CLI-" + String.format("%03d", contadorId++); // Formato CLI-001, CLI-002, etc.
+        this.nombreCompleto = nombreCompleto;
+        this.reservasActivas = new ArrayList<>();
+        this.historialReservas = new ArrayList<>();
+    }
+
+    //Getters
+    public String getId(){
+        return id;
+    }
+    public String getNombreCompleto(){
+        return nombreCompleto;
+    }
+    public List<Reserva> getReservasActivas(){
+        return reservasActivas;
+    }
+    public List<Reserva> getHistorialReservas(){
+        return historialReservas;
+    }
+
+    public void agregarReserva(Reserva reserva){
+        if (reservasActivas.size() >= 3) {
+            throw new IllegalStateException("El cliente ya tiene 3 reservas activas");
+        }
+        reservasActivas.add(reserva);
+    }
+    public void cancelarReserva(Reserva reserva){
+        reservasActivas.remove(reserva);
+        historialReservas.add(reserva);
+    }
+    @Override
+    public String toString() {
+        return "Cliente ID: " + id + " - " + nombreCompleto;
+    }
+}
